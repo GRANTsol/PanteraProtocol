dd3835b4b57ac568e7b8f16af17c7cb770d7340d
Grant <shsvsbsbsbdbd@gmail.com>
2025-05-11 14:20:37 -0400

    Update envConfig.js

--- backend/config/envConfig.js
+++ backend/config/envConfig.js
@@ -3,6 +3,14 @@ import dotenv from 'dotenv';
 import path from 'path';
 import Joi from 'joi';
 
+
+// config/envConfig.js
+export const envConfig = {
+  REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
+  REDIS_PORT: process.env.REDIS_PORT || '6379',
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || '',
+};
+
 // Load .env based on environment
 const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV || 'development'}`);
 dotenv.config({ path: envPath });
